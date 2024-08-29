8bf2b1c6fa3a97558c07ffc5b4edf00485688181
Mike <barbeque@users.noreply.github.com>
2024-08-28 20:43:48 -0600

    Dumped the entire ROM

--- dump.py
+++ dump.py
@@ -12,6 +12,13 @@ CHAR_HEIGHT = 16
 
 # [y * width + x]
 
+NUM_CHARS = 256 # number of chars in set to extract
+DUMP_COLUMNS = 16
+DUMP_ROWS = (NUM_CHARS // DUMP_COLUMNS)
+DUMP_WIDTH = CHAR_WIDTH * DUMP_COLUMNS
+DUMP_HEIGHT = CHAR_HEIGHT * DUMP_ROWS
+all = Image.new(mode='1', size=(DUMP_WIDTH, DUMP_HEIGHT))
+
 with open('CGROM60.60', 'rb') as f:
     rom = bytearray(f.read())
     a = BitArray(rom)
@@ -26,5 +33,10 @@ for character in range(0, 256):
             value = a[ptr + (row * CHAR_WIDTH) + column]
             x.putpixel((column, row), value)
 
+            all_x = int((character % DUMP_COLUMNS) * CHAR_WIDTH + column)
+            all_y = int((character // DUMP_ROWS) * CHAR_HEIGHT + row)
+            all.putpixel((all_x, all_y), value)
+
     formatted = format(character, '03')
     x.save(f'out-{formatted}.png')
+    all.save('all.png')
